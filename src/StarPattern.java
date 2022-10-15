811ce523fbf735f5139446d06a08479fa40377d3
MohitVermaCoder <verma.mohit95@outlook.com>
2022-10-15 20:21:29 +0530

    initial commit

---
+++ src/StarPattern.java
@@ -0,0 +1,18 @@
+
+public class StarPattern {
+	
+	public static void main(String[] args) {
+		
+		StringBuilder sb=new StringBuilder();
+		for(int i=0; i<5;i++) {
+			
+//			for(int j=0;j<=i;j++)
+//			{
+//				System.out.print("* ");
+//			}
+			sb.append("*");
+			System.out.println(sb);
+		}
+	}
+
+}
